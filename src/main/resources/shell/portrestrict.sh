47d6639a227ea2c7015887299b502690f2968257
Ning <ningyunfa@baidu.com>
2018-07-30 00:32:07 +0800

    修复sheell脚本

--- src/main/resources/shell/portrestrict.sh
+++ src/main/resources/shell/portrestrict.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-port = $1
-iptables -A INPUT -p udp -m state --state NEW -m udp --dport $port -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $port -j ACCEPT
+port=$1
+iptables -A INPUT -p udp -m state --state NEW -m udp --dport ${port} -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport ${port} -j ACCEPT
 service iptables save
 service iptables restart
